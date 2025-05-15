53aa9c8fc357e752ba36207c932bb1870e85eeaf
Anurag <0902cs221015@rjit.ac.in>
2025-05-16 03:57:39 +0530

    Updated Navbar.jsx Again

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -54,65 +54,7 @@ const Navbar = () => {
                 />
               </div>
             </div>
-          </Link>import { Link, useLocation } from "react-router";
-import useAuthUser from "../hooks/useAuthUser";
-import { BellIcon, LogOutIcon, ShipWheelIcon } from "lucide-react";
-import ThemeSelector from "./ThemeSelector";
-import useLogout from "../hooks/useLogout";
-
-const Navbar = () => {
-  const { authUser } = useAuthUser();
-  const location = useLocation();
-  const isChatPage = location.pathname?.startsWith("/chat");
-
-  const { logoutMutation } = useLogout();
-
-  return (
-    <nav className="bg-base-200 border-b border-base-300 sticky top-0 z-30 h-16 flex items-center">
-      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between w-full">
-          {isChatPage && (
-            <div className="pl-5">
-              <Link to="/" className="flex items-center gap-2.5">
-                <ShipWheelIcon className="size-9 text-primary" />
-                <span className="text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary  tracking-wider">
-                  EchoVerse
-                </span>
-              </Link>
-            </div>
-          )}
-          <div className="flex items-center gap-3 sm:gap-4">
-            <Link to={"/notifications"}>
-              <button className="btn btn-ghost btn-circle">
-                <BellIcon className="h-6 w-6 text-base-content opacity-70" />
-              </button>
-            </Link>
-            <ThemeSelector />
-            {authUser && (
-              <div className="flex items-center gap-3 sm:gap-4">
-                <Link to="/">
-                  <div className="avatar">
-                    <div className="w-8 sm:w-9 rounded-full">
-                      <img
-                        src={authUser?.profilePic}
-                        alt="User Avatar"
-                        rel="noreferrer"
-                      />
-                    </div>
-                  </div>
-                </Link>
-                <button className="btn btn-ghost btn-circle" onClick={logoutMutation}>
-                  <LogOutIcon className="h-6 w-6 text-base-content opacity-70" />
-                </button>
-              </div>
-            )}
-          </div>
-        </div>
-      </div>
-    </nav>
-  );
-};
-export default Navbar;
+          </Link>
 
           {/* Logout button */}
           <button className="btn btn-ghost btn-circle" onClick={logoutMutation}>
